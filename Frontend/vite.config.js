ee04bc9a39e18bfc54bf917052528e2bde6c8717
Shreyam Kundu <shreyamkundu04@gmail.com>
2024-04-06 19:44:15 +0530

    dotenv variable problem fixed

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,11 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   define: {
+//     'process.env': process.env
+//   }
+
+// })
+
+
+
+
+
+// import dotenv package
+import dotenv from 'dotenv';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// run package config
+dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+// define process env
   define: {
     'process.env': process.env
-  }
-
-})
+  }
+});
