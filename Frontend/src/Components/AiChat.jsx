6550b7fc684ad41b6bb60a37d4a28590c07b2b6c
snikdhendu <snikdhendupramanik@gmail.com>
2024-09-28 00:17:38 +0530

    Chatbot setup #2

---
+++ Frontend/src/Components/AiChat.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faComments } from '@fortawesome/free-solid-svg-icons';
+import Chatbot from '../Pages/Chatbot';
+
+
+const AiChat = ({ socketRef }) => {
+
+
+    return (
+
+        <div>
+
+            <button className="btn" onClick={() => document.getElementById('my_modal_3').showModal()}>open modal</button>
+            <dialog id="my_modal_3" className="modal">
+                <div className="modal-box  min-h-fit min-w-fit" >
+                    <form method="dialog">
+                        {/* if there is a button in form, it will close the modal */}
+                        <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                    </form>
+                    <div className=' w-full'>
+                        <Chatbot socket={socketRef}/>
+                    </div>
+                </div>
+            </dialog>
+        </div>
+    );
+}
+
+export default AiChat;
