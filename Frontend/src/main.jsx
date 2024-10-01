04c27cb4e4f1352bf4d1e850163286afe66cc948
Shreyam Kundu <shreyamkundu04@gmail.com>
2024-10-01 12:40:17 +0530

    react toasts added

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,12 +5,14 @@ import App from './App.jsx'
 import { store } from '../store.js'
 import './index.css'
 import { FirebaseProvider } from './Context/FirebaseContext.jsx'
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
  <Provider store={store}>
   <FirebaseProvider>
     <App />
+    <ToastContainer position='top-center' autoClose={2000} />
   </FirebaseProvider>
   </Provider>
 )
